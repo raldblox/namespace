93eb49f56f7ea798559e53a7a6a3d4f48813a073
raldblox <raldblox@outlook.com>
2023-05-03 03:15:58 +0800

    added: landing page and minting site

---
+++ dapp/pages/registrar/namespace.js
@@ -0,0 +1,143 @@
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+const namespace = () => {
+    const [root, setRoot] = useState("space");
+    const [name, setName] = useState("name");
+    const [chain, setChain] = useState("chain");
+
+    return (
+        <main>
+            <div className="z-10 w-full items-center justify-between font-mono text-base lg:flex">
+                <nav className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-100 py-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-100 lg:p-2 lg:dark:bg-zinc-800/30">
+                    <ul className='flex gap-2'>
+                        <li>
+                            <Link href="/">_home</Link>
+                        </li>
+                        <li>
+                            <Link href="/">_nsBrowser</Link>
+                        </li>
+                        <li>
+                            <Link href="/">_nsProfile</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <footer className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                    <p
+                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{' '}
+                        <span className=' font-black'>Zoociety</span>
+                    </p>
+                </footer>
+            </div>
+
+            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-fuchsia-300 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+                <p className='text-5xl font-bold'>{chain}://{name}.{root}</p>
+            </div>
+
+            <div className="mb-24 p-5 rounded-xl w-full lg:p-5 border border-gray-300 gap-5 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+                <div
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        1. Choose network
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+
+                        </span>
+                    </h2>
+                    <div className={`m-0 w-full grid text-sm gap-2`}>
+                        <select
+                            id="badge-class"
+                            value={chain}
+                            onChange={(e) => setChain(e.target.value)}
+                            className="w-full py-2 px-4"
+                        >
+                            <option value="">--Select Blockchain Network--</option>
+                            <option value="cic">
+                                CIC Chain Mainnet
+                            </option>
+                            <option value="mumbai" >Polygon Mumbai</option>
+                            <option value="polygon" >Polygon Mainnet</option>
+                            <option value="zkevm" disabled={true}>
+                                Polygon zkEVM
+                            </option>
+                            <option value="arbitrum" disabled={true}>
+                                Arbitrum One
+                            </option>
+                            <option value="optimism" disabled={true}>
+                                Optimism
+                            </option>
+                            <option value="ethereum" disabled={true}>
+                                Ethereum
+                            </option>
+
+                            <option value="bsc" disabled={true}>
+                                Binance Smart Chain
+                            </option>
+                        </select>
+
+
+                    </div>
+                </div>
+
+                <div
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        2. Create names
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+
+                        </span>
+                    </h2>
+                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                        <input
+                            onChange={(e) => setChain((e.target.value))}
+                            className='z-50 py-2 px-4 font-bold text-left border bg-white w-full' placeholder='insert username' />
+
+                    </p>
+                </div>
+
+                <div
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        3. Connect .rootdomain
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+
+                        </span>
+                    </h2>
+
+                    <input
+                        onChange={(e) => setRoot((e.target.value))}
+                        className='z-50 py-2 px-4 font-bold text-left border bg-white w-full' placeholder='insert rootdomain' />
+                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+
+                    </p>
+                </div>
+
+                <Link
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        4. Mint to own{' '}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+
+                        </span>
+                    </h2>
+                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+                       
+                    </p>
+                </Link>
+            </div>
+        </main>
+    )
+}
+
+export default namespace
---
+++ dapp/pages/registrar/rootdomain.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const rootdomain = () => {
+    return (
+        <main>rootdomain</main>
+    )
+}
+
+export default rootdomain
