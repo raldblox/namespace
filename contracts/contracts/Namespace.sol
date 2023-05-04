25f91a887df954839e9ccb3dc1f719f0102d608e
raldblox <raldblox@outlook.com>
2023-05-05 01:30:37 +0800

    add create name, create space, connect space

--- contracts/contracts/Namespace.sol
+++ contracts/contracts/Namespace.sol
@@ -2,11 +2,38 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+import "./NameStorage.sol";
+import "./SpaceStorage.sol";
+
 contract Namespace {
     address admin;
 
+    struct Name {
+        string[] names;
+        mapping(string => address) ownerOf;
+        mapping(string => string[]) spaces;
+        mapping(address => string[]) links;
+        mapping(address => string[]) files;
+    }
+
+    struct Space {
+        string[] spaces;
+        mapping(string => address) ownerOf;
+        mapping(string => string[]) names;
+        mapping(string => string[]) links;
+        mapping(string => string[]) files;
+    }
+
+    Name private name;
+    Space private space;
+
+    NameStorage private nameStorage;
+    SpaceStorage private spaceStorage;
+
     constructor() {
         admin = msg.sender;
+        nameStorage = new NameStorage();
+        spaceStorage = new SpaceStorage();
     }
 
     modifier onlyAdmin() {
@@ -16,5 +43,52 @@ contract Namespace {
 
     function isAdmin() public view returns (bool) {
         return admin == msg.sender;
+    }
+
+    function createName(string calldata _name) public {
+        require(name.ownerOf[_name] == address(0));
+        name.names.push(_name);
+        name.ownerOf[_name] = msg.sender;
+    }
+
+    function createSpace(string calldata _space) public {
+        require(space.ownerOf[_space] == address(0));
+        space.ownerOf[_space] = msg.sender;
+    }
+
+    function connectSpace(
+        string calldata _name,
+        string calldata _space
+    ) public {
+        require(
+            (name.ownerOf[_name] == msg.sender) ||
+                (name.ownerOf[_space] == msg.sender)
+        );
+        name.spaces[_name].push(_space);
+        space.names[_name].push(_name);
+    }
+
+    function getOwnerOfName(
+        string calldata _name
+    ) public view returns (address) {
+        return name.ownerOf[_name];
+    }
+
+    function getOwnerOfSpace(
+        string calldata _space
+    ) public view returns (address) {
+        return space.ownerOf[_space];
+    }
+
+    function getNameSpaces(
+        string calldata _name
+    ) public view returns (string[] memory) {
+        return name.spaces[_name];
+    }
+
+    function getSpaceNames(
+        string calldata _name
+    ) public view returns (string[] memory) {
+        return space.names[_name];
     }
 }
