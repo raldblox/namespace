8faa5c4452661b23430f3ae6534570e4edcaff96
raldblox <raldblox@outlook.com>
2023-05-04 13:36:22 +0800

    update footer

--- dapp/pages/index.js
+++ dapp/pages/index.js
@@ -25,15 +25,16 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <footer className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <p
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          <footer className="fixed bottom-0 left-0 flex h-32 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <a
+              className="pointer-events-none text-sm flex place-items-center gap-2 p-5 lg:pointer-events-auto lg:p-0"
+              href='https://www.zoociety.xyz'
               target="_blank"
               rel="noopener noreferrer"
             >
-              Powered by{' '}
-              <span className=' font-black'>Zoociety</span>
-            </p>
+              Made with ❤️ by{' '}
+              <a className='font-black' >ZoocietyLabs</a>
+            </a>
           </footer>
         </div>
 
