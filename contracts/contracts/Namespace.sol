213b16928b96b37698263288b11ccbaf914565ce
raldblox <raldblox@outlook.com>
2023-05-06 10:39:20 +0800

    update namespace

--- contracts/contracts/Namespace.sol
+++ contracts/contracts/Namespace.sol
@@ -3,7 +3,6 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 import "./NamespaceToken.sol";
-import "./interfaces/INamespace.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Namespace is INamespace {
@@ -26,6 +25,7 @@ contract Namespace is INamespace {
         string[] spaces;
         mapping(string => address) creator;
         mapping(string => string) orgnames;
+        mapping(string => string) orglogos;
         mapping(string => uint256) tokenIds;
         mapping(string => string) description;
         mapping(string => string[]) names;
@@ -39,10 +39,11 @@ contract Namespace is INamespace {
     NamespaceToken private token;
 
     mapping(address => string) primaryNames;
+    mapping(string => bool) isNames;
 
     constructor() {
         admin = msg.sender;
-        token = new NamespaceToken();
+        token = new NamespaceToken(msg.sender);
         setFees(1 ether, 1 ether, 1 ether);
     }
 
@@ -90,63 +91,90 @@ contract Namespace is INamespace {
     }
 
     function updateSpace(
-        string calldata _space,
+        string memory _space,
         string calldata _orgname,
         string calldata _description
-    ) public payable override {
-        require(space.creator[_space] == msg.sender, "Not authorized");
+    ) public payable {
+        if (!isAdmin()) {
+            uint256 tokenId = space.tokenIds[_space];
+            require(token.ownerOf(tokenId) == msg.sender, "Not authorized");
+        }
         space.orgnames[_space] = _orgname;
         space.description[_space] = _description;
     }
 
+    function updateLogo(
+        string calldata _space,
+        string calldata _logo
+    ) public payable {
+        if (!isAdmin()) {
+            uint256 tokenId = space.tokenIds[_space];
+            require(token.ownerOf(tokenId) == msg.sender, "Not authorized");
+        }
+        space.orglogos[_space] = _logo;
+    }
+
     function createName(
-        string calldata _name
-    ) public payable override returns (bool) {
+        string calldata _name,
+        address _owner
+    ) public payable returns (bool) {
         require(name.creator[_name] == address(0));
-        require(msg.value >= nameFee, "Name creation fee not met");
-        uint256 tokenId = token.mint(msg.sender, true);
+        if (!isAdmin()) {
+            require(msg.value >= nameFee, "Name creation fee not met");
+        }
+        uint256 tokenId = token.mint(_name, _owner, true);
         name.tokenIds[_name] = tokenId;
         name.names.push(_name);
-        name.creator[_name] = msg.sender;
+        name.creator[_name] = _owner;
+        isNames[_name] = true;
         setPrimary(_name);
         return true;
     }
 
     function createSpace(
+        address _owner,
         string calldata _space,
         string calldata _orgname,
-        string calldata _description
-    ) public payable override returns (bool) {
+        string calldata _description,
+        string calldata _logo
+    ) public payable returns (bool) {
         require(space.creator[_space] == address(0), "Space already taken");
-        require(msg.value >= spaceFee, "Space creation fee not met");
-        uint256 tokenId = token.mint(msg.sender, true);
+        if (!isAdmin()) {
+            require(msg.value >= spaceFee, "Space creation fee not met");
+        }
+        uint256 tokenId = token.mint(_space, _owner, false);
+        isNames[_space] = false;
         space.tokenIds[_space] = tokenId;
         space.spaces.push(_space);
-        space.creator[_space] = msg.sender;
+        space.creator[_space] = _owner;
+        space.orglogos[_space] = _logo;
         space.orgnames[_space] = _orgname;
         space.description[_space] = _description;
         return true;
     }
 
     function createNamespace(
+        address _owner,
         string calldata _name,
         string calldata _space
-    ) public payable override {
+    ) public payable {
         require(name.creator[_name] == address(0), "Name already taken.");
         require(space.creator[_space] != address(0), "Space doesn't exist");
-        uint256 totalFees = connectFee.add(space.membershipFees[_space]).add(
-            nameFee
-        );
-        require(msg.value >= totalFees, "Total fees not met.");
-        bool success = createName(_name);
-        require(success, "Failed to create name");
+        if (!isAdmin()) {
+            uint256 totalFees = connectFee
+                .add(space.membershipFees[_space])
+                .add(nameFee);
+            require(msg.value >= totalFees, "Total fees not met.");
+            bool success = createName(_name, _owner);
+            require(success, "Failed to create name");
+        }
         connectSpace(_name, _space);
     }
 
     function connectSpace(
         string calldata _name,
         string calldata _space
-    ) public payable override {
+    ) public payable {
         require(space.creator[_space] != address(0), "Space doesn't exist");
         require(name.creator[_name] != address(0), "Name doesn't exist");
         require(
@@ -154,16 +182,18 @@ contract Namespace is INamespace {
                 (space.creator[_space] == msg.sender),
             "Not authorized"
         );
-        require(
-            msg.value >= (connectFee.add(space.membershipFees[_space])),
-            "Connection fees not met"
-        );
-        (bool sent, ) = space.creator[_space].call{
-            value: space.membershipFees[_space]
-        }("");
-        require(sent, "Failed to send membership fee");
+        if (!isAdmin()) {
+            require(
+                msg.value >= (connectFee.add(space.membershipFees[_space])),
+                "Connection fees not met"
+            );
+            (bool sent, ) = space.creator[_space].call{
+                value: space.membershipFees[_space]
+            }("");
+            require(sent, "Failed to send membership fee");
+        }
         name.spaces[_name].push(_space);
-        space.names[_name].push(_name);
+        space.names[_space].push(_name);
     }
 
     function addLink(
@@ -171,7 +201,7 @@ contract Namespace is INamespace {
         string calldata _space,
         string memory _link,
         bool isName
-    ) public payable override {
+    ) public payable {
         address linkOwner;
         if (isName) {
             require(name.creator[_name] != address(0), "Name doesn't exist");
@@ -187,7 +217,7 @@ contract Namespace is INamespace {
     function hasSpace(
         string calldata _name,
         string calldata _space
-    ) public view override returns (bool) {
+    ) public view returns (bool) {
         bool foundSpace = false;
         for (uint i = 0; i < name.spaces[_name].length; i++) {
             if (
@@ -214,22 +244,22 @@ contract Namespace is INamespace {
     }
 
     function getNameSpaces(
-        string calldata _name
-    ) public view override returns (string[] memory) {
+        string memory _name
+    ) public view returns (string[] memory) {
         return name.spaces[_name];
     }
 
     function getSpaceNames(
-        string calldata _name
-    ) public view override returns (string[] memory) {
+        string memory _name
+    ) public view returns (string[] memory) {
         return space.names[_name];
     }
 
-    function getAllNames() public view override returns (string[] memory) {
+    function getAllNames() public view returns (string[] memory) {
         return name.names;
     }
 
-    function getAllSpaces() public view override returns (string[] memory) {
+    function getAllSpaces() public view returns (string[] memory) {
         return space.spaces;
     }
 
@@ -237,7 +267,7 @@ contract Namespace is INamespace {
         address owner,
         string memory _namespace,
         bool isName
-    ) public view override returns (string[] memory) {
+    ) public view returns (string[] memory) {
         if (isName) {
             return name.links[owner][_namespace];
         } else {
@@ -251,19 +281,100 @@ contract Namespace is INamespace {
 
     function getSpaceInfos(
         string memory _space
-    ) public view override returns (string memory, string memory) {
+    ) public view returns (string memory, string memory) {
         return (space.orgnames[_space], space.description[_space]);
     }
 
-    function resolveName(
-        string memory _name
-    ) public view override returns (address) {
+    function getSpaces(
+        string calldata _name
+    ) public view returns (string memory) {
+        string[] memory spaceNames;
+        bool isName = isNames[_name];
+        string memory spacesJson = "[";
+        string memory spaceJson;
+        if (isName) {
+            spaceNames = getNameSpaces(_name);
+            for (uint256 i = 0; i < spaceNames.length; i++) {
+                string memory spaceName = spaceNames[i];
+                string memory logo = space.orglogos[spaceName];
+                logo = space.orglogos[spaceName];
+
+                spaceJson = string(
+                    abi.encodePacked(
+                        '{"name":"',
+                        spaceName,
+                        '", "backgroundImage":"',
+                        logo,
+                        '"}'
+                    )
+                );
+                spacesJson = string(
+                    abi.encodePacked(
+                        spacesJson,
+                        spaceJson,
+                        i == spaceNames.length - 1 ? "" : ","
+                    )
+                );
+            }
+        } else {
+            spaceJson = string(
+                abi.encodePacked(
+                    '{"name":"',
+                    _name,
+                    '", "backgroundImage":"',
+                    space.orglogos[_name],
+                    '"}'
+                )
+            );
+            spacesJson = string(abi.encodePacked(spacesJson, spaceJson));
+        }
+
+        spacesJson = string(abi.encodePacked(spacesJson, "]"));
+
+        return spacesJson;
+    }
+
+    function getAttribute(
+        string memory name_
+    ) public view returns (string memory) {
+        bool isName = isNames[name_];
+        if (isName) {
+            return
+                string(
+                    abi.encodePacked(
+                        '{"trait_type": "TokenType", "value": "Name"},{"trait_type": "Connected Spaces", "value": "',
+                        isName
+                            ? Strings.toString((name.spaces[name_]).length)
+                            : "",
+                        '"}'
+                    )
+                );
+        } else {
+            string memory member = Strings.toString(
+                (getSpaceNames(name_).length)
+            );
+            return
+                string(
+                    abi.encodePacked(
+                        '{"trait_type": "Token Type", "value": "Space"},{"trait_type": "Space Description", "value": "',
+                        space.description[name_],
+                        '"},{"trait_type": "Space Name", "value": "',
+                        space.orgnames[name_],
+                        '"},{"trait_type": "Membership Fee", "value": "',
+                        Strings.toString(space.membershipFees[name_]),
+                        '"},{"trait_type": "Members Count", "value": "',
+                        member,
+                        '"}'
+                    )
+                );
+        }
+    }
+
+    function resolveName(string memory _name) public view returns (address) {
         return token.ownerOf(name.tokenIds[_name]);
     }
 
-    function resolveAddress(
-        address owner
-    ) public view override returns (string memory) {
+    function resolveAddress(address owner) public view returns (string memory) {
         return primaryNames[owner];
     }
 
@@ -272,6 +383,10 @@ contract Namespace is INamespace {
     }
 
     function tokenAddress() public view returns (address) {
+        return address(token);
+    }
+
+    function authorized() public view returns (address) {
         return address(token);
     }
 }
