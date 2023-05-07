548a9b37ec6b2b1d15410b3d0422773b99e70823
raldblox <raldblox@outlook.com>
2023-05-08 07:44:26 +0800

    add context

--- dapp/pages/_app.js
+++ dapp/pages/_app.js
@@ -1,12 +1,15 @@
 import Layout from "@/components/Layout";
+import { ContextProvider } from "@/context";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
-
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-
+    <ContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </ContextProvider>
   );
 }
