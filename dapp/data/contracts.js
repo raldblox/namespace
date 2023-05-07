c0c4ba3956bda8767b0bf8216fe6393932993b30
raldblox <raldblox@outlook.com>
2023-05-08 05:30:57 +0800

    update contracts

---
+++ dapp/data/contracts.js
@@ -0,0 +1,28 @@
+export const polygon = {
+  Namespace: "",
+  Token: "",
+  Themer: "",
+  Visual: "",
+};
+
+export const mumbai = {
+  Token: "0xA7FDFb03768d1f2419c966c84C23Fe7B084f6541",
+  Namespace: "0xc135E331A5802ff98F0E21a7A02e3b9FaFF284EA",
+};
+
+export const arbitrum = {
+  Token: "",
+  Namespace: "",
+};
+
+export const cic = {
+  Namespace: "0xdeb133D2687Cbe9844C23b89A52e10CAbdF6662F",
+  Visualizer: "0xA70E4B68B10154158235ED27247F315D7d6F22bD",
+  Themer: "0x6771Fd90f43C18628A3e7f37d4754Ff4e889B8ed",
+  Token: "0xb355511953Ff6DeBCE443730c9d471e3F6B9b209",
+};
+
+export const collection = {
+  mumbai: "https://testnets.opensea.io/collection/namespaces",
+  arbitrum: "",
+};
