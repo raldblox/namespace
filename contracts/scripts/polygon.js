f66979b667d0ab6929b88b5a374a7701a38b4f7d
raldblox <raldblox@outlook.com>
2023-05-11 01:51:32 +0800

    link-mint script

--- contracts/scripts/polygon.js
+++ contracts/scripts/polygon.js
@@ -1,8 +1,8 @@
+const fs = require("fs");
+const path = require("path");
 const { default: axios } = require("axios");
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-
   const { data } = await axios({
     method: "get",
     url: "https://gasstation-mainnet.matic.network/v2",
@@ -20,62 +20,100 @@ async function main() {
   console.log("MaxGas:", ethers.utils.formatEther(maxFeePerGas), "ETH");
   console.log("MaxFee:", ethers.utils.formatEther(maxPriorityFeePerGas), "ETH");
 
+  const [deployer] = await ethers.getSigners();
   console.log("Deployer:", deployer.address);
   console.log(
     "Balance:",
     ethers.utils.formatEther(await deployer.getBalance())
   );
 
-  const namespaceMainnet = "0xfe409ca6CaB3fbBb7c7372F17d84c3E5A94D06E9"; // deployed on mainnet on remix
-  const namespace = await ethers.getContractAt("Namespace", namespaceMainnet);
-  console.log("Mainnet Namespace:", namespace.address);
+  // DEPLOYMENTS ON POLYGON MAINNET
+  const Namespace = "0x44B0b339cFf4DC737ad3C35f33C6262e3Df45BDC";
+  const Token = "0xb374198d6D6b503Fa1f5B3908467F5CD9E7AeacE";
+  const Visualizer = "0x7C4671D11EAe4732b0c5D23437ae1c6e67bf06e1";
+  const Themer = "0x00D69D17444bEE3a861a052ac9d572F7B472d548";
 
-  // Deploy Namespace Contract
-  // const Namespace = await ethers.getContractFactory("Namespace");
-  // const namespace = await Namespace.deploy();
-  // await namespace.deployed();
-  // console.log("Namespace:", namespace.address);
+  const namespace = await ethers.getContractAt("Namespace", Namespace);
+  const token = await ethers.getContractAt("NamespaceToken", Token);
+  const visualizer = await ethers.getContractAt("Visualizer", Visualizer);
+  const themer = await ethers.getContractAt("Namespace", Themer);
 
-  // Deploy Token Contract
-  const NamespaceToken = await ethers.getContractFactory("NamespaceToken");
-  const token = await NamespaceToken.deploy({
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-  });
-  await token.deployed();
+  console.log("Namespace:", namespace.address);
   console.log("Token:", token.address);
-
-  // Link Namespace to Token Contract
-  await namespace.setToken(token.address);
-  await token.setNamespace(namespace.address);
-
-  // Add NFT Themer to Token Contract
-  const Themer = await ethers.getContractFactory("Themer");
-  const themer = await Themer.deploy({
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-  });
-  await themer.deployed();
   console.log("Themer:", themer.address);
-
-  // Add NFT Visualizer to Token Contract
-  const Visualizer = await ethers.getContractFactory("Visualizer");
-  const visualizer = await Visualizer.deploy({
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-  });
-  await visualizer.deployed();
   console.log("Visualizer:", visualizer.address);
 
+  // Link All Contracts
+  await namespace.setToken(token.address, {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  console.log("Token is connected to namespace.");
+  await token.setNamespace(namespace.address, {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  console.log("Namespace is connected to token.");
   await token.setThemer(themer.address, {
     maxFeePerGas,
     maxPriorityFeePerGas,
   });
+  console.log("Themer is connected to token.");
   await token.setVisualizer(visualizer.address, {
     maxFeePerGas,
     maxPriorityFeePerGas,
   });
-  console.log("Verify Token:", await namespace.tokenAddress());
+  console.log("Visualizer is connected to token.");
+
+  // CREATE SPACES
+  await namespace.createSpace(
+    deployer.address,
+    "space",
+    "namespace",
+    "Connect, create, and control with Namespace.",
+    "https://zoociety.xyz/assets/namespace.png",
+    true,
+    {
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    }
+  );
+  await namespace.createSpace(
+    deployer.address,
+    "blox",
+    "BLOXIE",
+    "A space full of blo[x]es",
+    "https://zoociety.xyz/assets/me.png",
+    true,
+    {
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    }
+  );
+  await namespace.createSpace(
+    deployer.address,
+    "zoociety",
+    "ZOOCIETY",
+    "Synergistic Ecosystem of Emerging Technology",
+    "https://zoociety.xyz/assets/zoociety.png",
+    true,
+    {
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    }
+  );
+  await namespace.createSpace(
+    deployer.address,
+    "badge",
+    "BADGIFY",
+    "We make awesome digital badges on the blockchain",
+    "https://zoociety.xyz/assets/badgify.png",
+    true,
+    {
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    }
+  );
 
   await namespace.createName("rald", deployer.address, {
     maxFeePerGas,
@@ -98,50 +136,6 @@ async function main() {
     maxPriorityFeePerGas,
   });
 
-  await namespace.createSpace(
-    deployer.address,
-    "blox",
-    "RALDBLOX",
-    "A space full of blo[x]",
-    "https://zoociety.xyz/assets/me.png",
-    {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    }
-  );
-  await namespace.createSpace(
-    deployer.address,
-    "zoociety",
-    "ZOOCIETY",
-    "Synergistic Ecosystem of Emerging Technology",
-    "https://zoociety.xyz/assets/zoociety.png",
-    {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    }
-  );
-  await namespace.createSpace(
-    deployer.address,
-    "badge",
-    "BADGIFY",
-    "We make awesome digital badges on the blockchain",
-    "https://zoociety.xyz/assets/badgify.png",
-    {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    }
-  );
-  await namespace.createSpace(
-    deployer.address,
-    "space",
-    "namespace",
-    "Unleashing Potential of Digital Identity",
-    "https://zoociety.xyz/assets/namespace.png",
-    {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    }
-  );
   await namespace.connectSpace("rald", "blox", {
     maxFeePerGas,
     maxPriorityFeePerGas,
@@ -170,10 +164,28 @@ async function main() {
     maxFeePerGas,
     maxPriorityFeePerGas,
   });
-  await token.setColor(1, "white", "blue", {
+
+  await token.setColor(1, "#131313", "#94ff2b", {
     maxFeePerGas,
     maxPriorityFeePerGas,
   });
+  await token.setColor(2, "#f0f8ff", "#ff0000", {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  await token.setColor(3, "#131313", "#00ff7f", {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  await token.setColor(4, "#131313", "#3399ff", {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+  await token.setColor(5, "#131313", "#00ffff", {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+  });
+
   console.log("Token Supply:", await namespace.tokenSupply());
   console.log("Member Count:", await namespace.getSpaceNames("blox"));
   console.log("All Spaces:", await namespace.getAllSpaces());
