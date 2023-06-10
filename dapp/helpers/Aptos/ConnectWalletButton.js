47899751d9165e4ea1d850805410ccd03a13838b
raldblox <raldblox@outlook.com>
2023-06-10 21:59:02 +0800

    Updated imports

--- dapp/helpers/Aptos/ConnectWalletButton.js
+++ dapp/helpers/Aptos/ConnectWalletButton.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import styles from '../../styles/Home.module.css'
 
-import {useWallet} from "@manahippo/aptos-wallet-adapter"
+import { useWallet } from "@manahippo/aptos-wallet-adapter"
 
 import ConnectWalletModal from "./ConnectWalletModal"
 
 const ConnectWalletButton = (props) => {
-    const {connectButton, className, style, disabled} = props
+    const { connectButton, className, style, disabled } = props
 
     const wallet = useWallet()
     const [showModal, setShowModal] = useState(false)
@@ -19,14 +18,14 @@ const ConnectWalletButton = (props) => {
         wallet.disconnect()
     }
 
-    const button = <button disabled={disabled} className={`${styles.connectWalletBtn} ${className} ${disabled ? "disabled" : ""}`} onClick={handleButtonClick} style={style}>
+    const button = <button disabled={disabled} className={` ${className} ${disabled ? "disabled" : ""}`} onClick={handleButtonClick} style={style}>
         {connectButton ? "Connect Wallet" : "Disconnect"}
     </button>
 
     return (
         <>
-        {connectButton ? button : wallet.account?.address?.toString() !== undefined ? <span className="mx-auto w-100">{button}</span> : null}
-        <ConnectWalletModal show={showModal} onConnect={() => setShowModal(false)} />
+            {connectButton ? button : wallet.account?.address?.toString() !== undefined ? <span className="mx-auto w-100">{button}</span> : null}
+            <ConnectWalletModal show={showModal} onConnect={() => setShowModal(false)} />
         </>
     )
 }
