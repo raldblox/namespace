6309b1f2d00db7b68c0eaabb9d8006a1c89375cf
raldblox <raldblox@outlook.com>
2023-06-28 03:16:10 +0800

    new typescript web app enviro

---
+++ web-app/data/contracts.js
@@ -0,0 +1,30 @@
+export const polygon = {
+  Namespace: "0x44B0b339cFf4DC737ad3C35f33C6262e3Df45BDC",
+  Token: "0xb374198d6D6b503Fa1f5B3908467F5CD9E7AeacE",
+  Themer: "0x00D69D17444bEE3a861a052ac9d572F7B472d548",
+  Visualizer: "0x7C4671D11EAe4732b0c5D23437ae1c6e67bf06e1",
+};
+
+export const mumbai = {
+  Namespace: "0xBa2BF82362586f1048f105926C219185bbdE942f",
+  Token: "0x27d93c74aa31EbdF197BA5ecbB0533373054978a",
+  Themer: "0xBdd7Af1a8C5d3dF9DEe605510733FAC9eFe8f283",
+  Visualizer: "0xbd90E6315bB0AA95704c2e246c5dEA93112b83Ad",
+};
+
+export const arbitrum = {
+  Token: "",
+  Namespace: "",
+};
+
+export const cic = {
+  Namespace: "0xdeb133D2687Cbe9844C23b89A52e10CAbdF6662F",
+  Visualizer: "0xA70E4B68B10154158235ED27247F315D7d6F22bD",
+  Themer: "0x6771Fd90f43C18628A3e7f37d4754Ff4e889B8ed",
+  Token: "0xb355511953Ff6DeBCE443730c9d471e3F6B9b209",
+};
+
+export const collection = {
+  mumbai: "https://testnets.opensea.io/collection/namespaces",
+  arbitrum: "",
+};
