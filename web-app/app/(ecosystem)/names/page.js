67beceeb6089770d90d8031e17822302eef216aa
raldblox <raldblox@outlook.com>
2023-07-19 21:48:25 +0800

    added new pages

---
+++ web-app/app/(ecosystem)/names/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default index
---
+++ web-app/app/(ecosystem)/spaces/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const index = () => {
+  return <section>
+    
+  </section>;
+};
+
+export default index;
