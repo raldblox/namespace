38f549d4d7218cbeb7544507120c7941b142c104
raldblox <raldblox@outlook.com>
2023-08-16 22:37:05 +0800

    init namespace periphery and utilitycontract

---
+++ solidity-workspace/contracts/NamespacePeriphery/NamespaceLink..sol
@@ -0,0 +1,18 @@
+/**
+ *
+ * @title NAMESPACE V2
+ * @author raldblox.eth
+ *
+ */
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/// @title Namespace Registry
+/// @author raldblox.eth
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+contract NamespaceRegistry {
+    constructor() {}
+}
