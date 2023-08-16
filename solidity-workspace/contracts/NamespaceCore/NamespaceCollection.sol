fa16b0f0c005669b077b0a8b95a009e96e8ad810
raldblox <raldblox@outlook.com>
2023-08-16 22:35:48 +0800

    init namespace token core contracts

---
+++ solidity-workspace/contracts/NamespaceCore/NamespaceCollection.sol
@@ -0,0 +1,18 @@
+/**
+ *
+ * @title NAMESPACE V2
+ * @author raldblox.eth
+ *
+ */
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/// @title Namespace Collection
+/// @author raldblox.eth
+/// @notice
+/// @dev
+contract NamespaceCollection {
+    constructor() {}
+}
---
+++ solidity-workspace/contracts/NamespaceCore/NamespaceRegistry.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/// @title Namespace Collection
+/// @author raldblox.eth
+/// @notice
+/// @dev
+contract NamespaceRegistry {
+    constructor() {}
+}
