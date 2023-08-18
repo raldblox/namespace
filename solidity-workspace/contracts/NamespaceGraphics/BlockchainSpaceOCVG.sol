3256c81b64bbe9ab4365f19747f4452cfed0da8d
raldblox <raldblox@outlook.com>
2023-08-18 09:00:31 +0800

    initiliaze namespace visual graphics

---
+++ solidity-workspace/contracts/NamespaceGraphics/BlockchainSpaceOCVG.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "../libraries/OCVG.sol";
+
+/// @title Namespace Registry Core Contract
+/// @author raldblox.eth
+/// @notice Handle of all Name, Space, and Namespace Records
+/// @dev The Namespace Storage Contract
+contract BlockchainSpaceOCVG {
+    
+}
---
+++ solidity-workspace/contracts/NamespaceGraphics/NamespaceOCVG.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@zoociety/nifty-contracts/contracts/onchainvision/libraries/OCVG.sol";
+
+contract NamespaceOCVG {}
