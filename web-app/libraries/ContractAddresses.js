761eac99b46aa65ec31dba1df3759e1125d8194c
raldblox <raldblox@outlook.com>
2023-08-21 10:17:19 +0800

    added: testnet contract deployment

---
+++ web-app/libraries/ContractAddresses.js
@@ -0,0 +1,46 @@
+export const ContractAddresses = {
+    "polygon": {
+        mumbai: [{
+            namespaceRegistry: "0x7d65FE8A1Cb67222b0c6113cacAd9C8267C20b4c",
+            blockchainName: "0x307cB1808497026ab93908E9156B2eedc8448e85",
+            blockchainSpace: "0x5D51F8321F5aeA1dC117900595BFA67579372Cfa",
+            namespaceOCVG: "0xBC83F42651f793026CC645B4e02455ceB1E26304",
+            nameOCVG: "0x60D296800F8f423F211F7FcA196aF2D22A356f4D",
+            spaceOCVG: "0x9d0d2744646c9e561bf926dfD2E4CA7B1101d418",
+        }],
+        mainnet: [{
+            namespaceRegistry: "",
+            blockchainName: "",
+            blockchainSpace: "",
+            namespaceOCVG: "",
+            nameOCVG: "",
+            spaceOCVG: "",
+        }],
+        zkevm: [{
+            namespaceRegistry: "",
+            blockchainName: "",
+            blockchainSpace: "",
+            namespaceOCVG: "",
+            nameOCVG: "",
+            spaceOCVG: "",
+        }]
+    },
+    "patex": {
+        sepholia: [{
+            namespaceRegistry: "0x6232A1815873dB7a74e75ad623a412b2c6F3097b",
+            blockchainName: "0xE433fF12151b4435e8C043E4c7b420eeda9e33bd",
+            blockchainSpace: "0x2E7e74fcA1491c8df397665cB326fcCd10D8E433",
+            namespaceOCVG: "0x42F11590Cd4C5606a7d81cC264Aa3A1307C22f52",
+            nameOCVG: "0xb328d51175Af8e0Bb7856784991ABa84387f86a0",
+            spaceOCVG: "0x4823CFB099C2B6F6F994Ebe2dFa11Ed373437A2a",
+        }],
+        mainnet: [{
+            namespaceRegistry: "",
+            blockchainName: "",
+            blockchainSpace: "",
+            namespaceOCVG: "",
+            nameOCVG: "",
+            spaceOCVG: "",
+        }],
+    },
+}
