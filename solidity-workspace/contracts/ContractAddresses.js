af9f36d079bedadfe637f8fc66798ad7087dd3ef
raldblox <raldblox@outlook.com>
2023-08-23 18:21:20 +0800

    added deployed contracts on pego-testnet

--- solidity-workspace/contracts/ContractAddresses.js
+++ solidity-workspace/contracts/ContractAddresses.js
@@ -139,4 +139,22 @@ export const ContractAddresses = {
             spaceOCVG: "",
         }],
     },
+    "pego": {
+        mainnet: [{
+            namespaceRegistry: "",
+            blockchainName: "",
+            blockchainSpace: "",
+            namespaceOCVG: "",
+            nameOCVG: "",
+            spaceOCVG: "",
+        }],
+        testnet: [{
+            namespaceRegistry: "0x6232A1815873dB7a74e75ad623a412b2c6F3097b",
+            blockchainName: "0xE433fF12151b4435e8C043E4c7b420eeda9e33bd",
+            blockchainSpace: "0x2E7e74fcA1491c8df397665cB326fcCd10D8E433",
+            namespaceOCVG: "0x42F11590Cd4C5606a7d81cC264Aa3A1307C22f52",
+            nameOCVG: "0xb328d51175Af8e0Bb7856784991ABa84387f86a0",
+            spaceOCVG: "0x4823CFB099C2B6F6F994Ebe2dFa11Ed373437A2a",
+        }],
+    },
 }
