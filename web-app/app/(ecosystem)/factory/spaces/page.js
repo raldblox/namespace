fb132b80e1ff76a2dc71d26a871ca302ebde8cb8
raldblox <raldblox@outlook.com>
2023-08-24 11:23:36 +0800

    updated layout and border

--- web-app/app/(ecosystem)/factory/spaces/page.js
+++ web-app/app/(ecosystem)/factory/spaces/page.js
@@ -5,8 +5,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <section className='grid items-start justify-start min-h-[90vh] grid-cols-1 lg:grid-cols-2 gap-8 p-4 lg:p-8'>
-      <div className='grid col-span-1 gap-8 py-8'>
+    <section className='grid items-start justify-start min-h-[90vh] grid-cols-1 lg:grid-cols-2 gap-8 py-4 lg:py-8'>
+      <div className='grid col-span-1 gap-8 px-4 lg:px-8 lg:py-8'>
         <a
           href="https://dorahacks.io/aptos/round-3/buidl"
           target="_blank"
@@ -38,7 +38,7 @@ const page = () => {
           </button>
         </div>
       </div>
-      <div className="grid content-end w-full h-full grid-cols-2 col-span-1 py-8 gap-4 lg:grid-cols-3 lg:pt-[40vh]">
+      <div className="grid px-4 lg:px-8 content-end w-full h-full grid-cols-2 col-span-1 py-8 gap-4 lg:grid-cols-3 lg:pt-[40vh]">
         <h2 className='col-span-2 py-8 text-2xl lg:text-4xl lg:col-span-3'>Recently Created Spaces</h2>
         <NsCard chain="ETHEREUM" tokenId="1" spaceTld=".ape" spaceName="APE COMMUNITY" spaceDesc="We ape-ing here. Wanna join?" spaceCover="" />
         <NsCard chain="OPTIMISM" tokenId="123" spaceTld=".xyz" spaceName="XYZ Innovators" spaceDesc="Exploring the unknown in the world of XYZ." />
@@ -48,14 +48,18 @@ const page = () => {
         <NsCard chain="AVAX" tokenId="789" spaceTld=".dream" spaceName="Dreamer's Hub" spaceDesc="Where dreams turn into reality, one project at a time." />
 
       </div>
-      <div className="grid content-end w-full h-full grid-cols-2 col-span-2 gap-4 py-8 border-t-2 border-black lg:grid-cols-6">
-        <h2 className='col-span-2 py-8 text-2xl lg:text-4xl lg:col-span-6'>Top Blockchain Spaces</h2>
-        <NsCard chain="PATEX" tokenId="101" spaceTld=".stars" spaceName="Stargazers Society" spaceDesc="Exploring the universe through technology and curiosity." />
-        <NsCard chain="BNB" tokenId="303" spaceTld=".crypto" spaceName="CryptoPioneers" spaceDesc="For those leading the way in the world of crypto." />
-        <NsCard chain="XDC" tokenId="202" spaceTld=".innovation" spaceName="Tech Innovation Hub" spaceDesc="Fostering innovation and creativity in the tech world." />
-        <NsCard chain="EARTH" tokenId="42" spaceTld=".wander" spaceName="Wanderlust Dreamers" spaceDesc="Embark on global adventures and share travel stories." />
-        <NsCard chain="PIXEL" tokenId="123" spaceTld=".art" spaceName="Pixel Perfectionists" spaceDesc="Celebrating the beauty of digital art, one pixel at a time." />
-        <NsCard chain="DREAM" tokenId="777" spaceTld=".fantasy" spaceName="Fantasy Realm Explorers" spaceDesc="Unveiling the magical mysteries of enchanting worlds." />      </div>
+      <div className='col-span-1 border-t-2 border-black lg:col-span-2 '>
+        <div className="grid content-end w-full h-full grid-cols-2 gap-4 px-4 lg:px-8 lg:py-8 lg:grid-cols-6">
+          <h2 className='col-span-2 py-8 text-2xl lg:text-4xl lg:col-span-6'>Top Blockchain Spaces</h2>
+          <NsCard chain="PATEX" tokenId="101" spaceTld=".stars" spaceName="Stargazers Society" spaceDesc="Exploring the universe through technology and curiosity." />
+          <NsCard chain="BNB" tokenId="303" spaceTld=".crypto" spaceName="CryptoPioneers" spaceDesc="For those leading the way in the world of crypto." />
+          <NsCard chain="XDC" tokenId="202" spaceTld=".innovation" spaceName="Tech Innovation Hub" spaceDesc="Fostering innovation and creativity in the tech world." />
+          <NsCard chain="EARTH" tokenId="42" spaceTld=".wander" spaceName="Wanderlust Dreamers" spaceDesc="Embark on global adventures and share travel stories." />
+          <NsCard chain="PIXEL" tokenId="123" spaceTld=".art" spaceName="Pixel Perfectionists" spaceDesc="Celebrating the beauty of digital art, one pixel at a time." />
+          <NsCard chain="DREAM" tokenId="777" spaceTld=".fantasy" spaceName="Fantasy Realm Explorers" spaceDesc="Unveiling the magical mysteries of enchanting worlds." />
+        </div>
+      </div>
+
     </section>
   )
 }
