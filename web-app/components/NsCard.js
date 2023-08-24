b8e9e075d31134bb5d895346f165047d20377d31
raldblox <raldblox@outlook.com>
2023-08-24 12:51:13 +0800

    adjusted layout

--- web-app/components/NsCard.js
+++ web-app/components/NsCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NsCard = ({ chain, tokenId, spaceTld, spaceName, spaceDesc, spaceCover }) => {
     return (
         <div
-            className="gap-4 content-between grid p-4 aspect-square bg-dark min-h-[20vh] w-full group rounded-md"
+            className="gap-1 lg:gap-4 content-between grid p-4 aspect-square bg-dark min-h-[20vh] w-full group rounded-md"
         >
             <p className="flex justify-between text-sm font-bold text-white">
                 {chain}
@@ -11,7 +11,7 @@ const NsCard = ({ chain, tokenId, spaceTld, spaceName, spaceDesc, spaceCover }) 
                     {tokenId}
                 </span>
             </p>
-            <p className="text-lg accent space group-hover:text-white">
+            <p className="text-base lg:text-lg accent space group-hover:text-white">
                 {spaceTld}
                 <span className="inline-block pl-2 transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                     &gt;
@@ -19,7 +19,7 @@ const NsCard = ({ chain, tokenId, spaceTld, spaceName, spaceDesc, spaceCover }) 
             </p>
             <div className='hidden group-hover:grid'>
                 <p className='text-sm lg:text-base group-hover:accent'>{spaceName}</p>
-                <p className='overflow-y-scroll text-xs'>{spaceDesc}</p>
+                <p className='text-xs'>{spaceDesc}</p>
             </div>
         </div>
     )
